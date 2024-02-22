fc61025fe31f0d1c2f45bcbf4d6084482d61a2ac
Paco De Luis <ponchoflores464@gmail.com>
2024-02-22 14:24:47 -0600

    05 Implements Cloudinary

--- library/models.py
+++ library/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from cloudinary.models import CloudinaryField
+
 # Create your models here.
 class Module(models.Model):
     name = models.CharField(
@@ -27,13 +29,21 @@ class Question(models.Model):
         Module,
         on_delete = models.CASCADE,
         verbose_name = "Módulo")
+    
     question_text = models.TextField(
         verbose_name = "Texto de la Pregunta",
         null = True, blank = True)
-    question_image = models.ImageField(
-        verbose_name = "Imagen de la Pregunta",
-        upload_to = 'questions',
-        null = True, blank = True)
+    # question_image = models.ImageField(
+    #     verbose_name = "Imagen de la Pregunta",
+    #     upload_to = 'questions',
+    #     null = True, blank = True)
+
+    question_image = CloudinaryField(
+        verbose_name = "Imagen de la pregunta",
+        null = True, blank = True,
+        resource_type = "image",
+        folder = "questions"
+    )
     answer1 = models.CharField(
         verbose_name = "Respuesta A",
         max_length = 200)
